1b8a9f357418674b3434d554ef82ebb7b3cec665
8Lou <lou8@mail.ru>
2023-12-18 11:13:18 +0300

    page_2

--- js/main.js
+++ js/main.js
@@ -1,20 +1,12 @@
-//  var swiper = new Swiper(".swiper", {
-//       effect: "cube",
-//       grabCursor: true,
-//       cubeEffect: {
-//         shadow: true,
-//         slideShadows: true,
-//         shadowOffset: 20,
-//         shadowScale: 0.94,
-//       },
-//       pagination: {
-//         el: ".swiper-pagination",
-//       },
-//     });
-    
+
     const swiper = new Swiper(".swiper", {
+      effect: 'fade',
       pagination: {
         el: ".swiper-pagination",
         hide: true,
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
       },
     });
